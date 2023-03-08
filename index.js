fcdba906e6a3f6d5e28ac2d7afff5cde0da910b0
Tom ROBERTO <tom.roberto31@gmail.com>
2023-03-08 13:47:51 +0100

    domain  2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post("/cookie", (req, res) => {
   res
     .cookie("token", "1234567890", {
       expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
+      //   httpOnly: true,
       sameSite: "none",
       secure: true,
       domain: "magenta-dolphin-7f08eb.netlify.app",
