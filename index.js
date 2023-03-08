98ff1fe6a4e5260007565ebe9b3fede22a09ed6b
Tom ROBERTO <tom.roberto31@gmail.com>
2023-03-08 12:04:21 +0100

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+
+app.post("/cookie", (req, res) => {
+  res
+    .cookie("token", "1234567890", {
+      expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    })
+    .json({ message: "cookie ?" });
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("server started borrrrr");
+});
