e16151c05ca5ee8bd35b090e634e574ee14be861
Tom ROBERTO <tom.roberto31@gmail.com>
2023-03-08 14:15:56 +0100

    domain  from front

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.use(
     origin: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post("/cookie", (req, res) => {
   res
